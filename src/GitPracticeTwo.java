6f62be6f4261e69c2ed1ef9755a61191f5c752e0
MirSirat <mirahmad.sirat@tekschool.us>
2022-06-22 17:38:42 -0700

    new method

--- src/GitPracticeTwo.java
+++ src/GitPracticeTwo.java
@@ -1,9 +1,10 @@
 
 public class GitPracticeTwo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		public int sum (int a , int b) {
+			int sum = a+b;
+			return sum;
+		}
 	}
 
-}
+
